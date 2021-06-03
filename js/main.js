be7f6b57ff5dbcc3a90b430f67da1edce25cb381
meng <loh.yumeng@gmail.com>
2021-06-03 20:56:03 +1000

    first draft, responsive unimplemented

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+});
+
+window.addEventListener("hashchange", (evt) => {
+  const navitems = document.getElementsByClassName('navitem');
+  const sections = document.getElementsByTagName('section');
+
+  for (let x of navitems)
+    x.classList.remove("active");
+
+  for (let x of sections)
+    x.classList.remove("display");
+
+  const id = `navitem-${location.hash.substr(1)}`;
+  const section_id = `section-${location.hash.substr(1)}`;
+
+  document.getElementById(id).classList.add('active');
+
+  document.getElementById(section_id).classList.add('display');
+  console.log(`section_id: ${section_id}`);
+
+});
