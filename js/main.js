96eb2ce0e2eb77007425b1bc9bb4a458394f7973
meng <loh.yumeng@gmail.com>
2021-06-07 12:30:38 +1000

    Background change; slide anim fix; add hamburger
    - Changed background to conference hall.
    - fixed sections sliding off screen having wrong sizing/position
    - added hamburger menu for smaller screens

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const sectionLoader = (evt) => {
+const sectionLoader = evt => {
   const navitems = document.getElementsByClassName('navitem');
   const sections = document.getElementsByTagName('section');
 
@@ -11,13 +11,22 @@ const sectionLoader = (evt) => {
   const id = `navitem-${location.hash.substr(1)}`;
   const section_id = `section-${location.hash.substr(1)}`;
 
-  document.getElementById(id).classList.add('active');
+  for (let elem of document.getElementsByClassName(id))
+    elem.classList.add('active');
 
   document.getElementById(section_id).classList.add('display');
   console.log(`section_id: ${section_id}`);
 };
 
+const hamburgerMouseOver = () => {
+  document.querySelector('.hamburger').addEventListener('click', evt => {
+    const navVert = document.querySelector('.navlist-vert');
+    const isShowing = navVert.classList.contains('show');
+    isShowing ? navVert.classList.remove('show') : navVert.classList.add('show');
+  });
+};
 
 window.addEventListener("load", sectionLoader);
+window.addEventListener("load", hamburgerMouseOver);
 
 window.addEventListener("hashchange", sectionLoader);
