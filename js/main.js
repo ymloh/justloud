f9aa18b67659c7eb69bd81b6a39a87b6fef084a2
meng <loh.yumeng@gmail.com>
2021-06-07 13:34:42 +1000

    standard background class extracted; collapse menu tweaked

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ const sectionLoader = evt => {
     elem.classList.add('active');
 
   document.getElementById(section_id).classList.add('display');
-  console.log(`section_id: ${section_id}`);
+  // console.log(`section_id: ${section_id}`);
 };
 
-const hamburgerMouseOver = () => {
+const hamburgerClick = () => {
   document.querySelector('.hamburger').addEventListener('click', evt => {
     const navVert = document.querySelector('.navlist-vert');
     const isShowing = navVert.classList.contains('show');
@@ -26,7 +26,14 @@ const hamburgerMouseOver = () => {
   });
 };
 
+const hamburgerMenuItemClick = () => {
+  const navlist_vert = document.querySelector('.navlist-vert');
+  for (let elem of document.querySelectorAll('.hamburger-nav a'))
+    elem.addEventListener('click', () => navlist_vert.classList.remove('show'));
+}
+
 window.addEventListener("load", sectionLoader);
-window.addEventListener("load", hamburgerMouseOver);
+window.addEventListener("load", hamburgerClick);
+window.addEventListener("load", hamburgerMenuItemClick);
 
 window.addEventListener("hashchange", sectionLoader);
